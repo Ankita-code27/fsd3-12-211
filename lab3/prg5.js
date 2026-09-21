e3effa6e028fa799ec06f10fe9a0dd3030efd3d4
Ankita Gupta <ankitakgupta27@gmail.com>
2026-09-21 21:34:33 +0530

    file updated

--- lab3/prg5.js
+++ lab3/prg5.js
@@ -25,4 +25,4 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => console.log("Server runs in 3000..."));
+server.listen(3000, () => console.log("Server runs in 3000...."));
