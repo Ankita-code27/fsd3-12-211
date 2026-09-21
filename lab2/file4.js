31e2182e3af681f640e646e78f00874435e3e3d8
Ankita Gupta <ankitakgupta27@gmail.com>
2026-09-21 21:28:35 +0530

    file updated

--- lab2/file4.js
+++ lab2/file4.js
@@ -3,7 +3,7 @@ import { mkdir, rm } from "fs/promises";
 //Create recursive directory
 await mkdir("uploads/resume", { recursive: true });
 
-//Create Single directory
+//Create single directory
 await mkdir("uploads/images");
 
 //remove directory
